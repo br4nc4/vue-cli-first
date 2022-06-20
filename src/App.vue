e9421cf86279a2587b2eacca9988930aa9d11499
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-06-20 18:57:58 +0200

    first app pt.1 restarting after deleted repository

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <TitlePage></TitlePage>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TitlePage from './components/TitlePage.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    components:{
+        TitlePage,
+    }
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
